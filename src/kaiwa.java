4eaaaa3d6bd803aa576ff00efba3b926a753972a
t16cs030 <mizuking0w0@gmail.com>
2018-12-12 22:48:20 +0900

    fix
    会話中スキップすると音楽が止まらないバグを修正
    テキストの読み込みができないバグを修正

--- src/kaiwa.java
+++ src/kaiwa.java
@@ -24,7 +24,7 @@ public class kaiwa implements State  {
 	int nameflag=1;
 	int tekitime=0;
 	int sound=0;
-	
+
 
 	Image kaifuku;
 
@@ -32,10 +32,10 @@ public class kaiwa implements State  {
 	private AudioClip hati;
 	private AudioClip setumei;
 	private AudioClip e;
-	
 
-	 
-	 
+
+
+
 	  String lineSepa=System.getProperty("line.separator");
 
 	public kaiwa(Model model){
@@ -48,16 +48,16 @@ public class kaiwa implements State  {
 		  bousikuro = Toolkit.getDefaultToolkit().getImage(getClass().getResource("bousiyakuro.png"));
 		  honey = Toolkit.getDefaultToolkit().getImage(getClass().getResource("honey1.png"));
 		  kaifuku = Toolkit.getDefaultToolkit().getImage(getClass().getResource("kaifuku1.png"));
-		
+
 		  aris = Applet.newAudioClip(getClass().getResource("aris.wav"));
 		  hati = Applet.newAudioClip(getClass().getResource("hati.wav"));
 		  setumei = Applet.newAudioClip(getClass().getResource("setumei.wav"));
 		  e = Applet.newAudioClip(getClass().getResource("e.wav"));
-		
+
 
 		   aris.play();
 		   Arrays.fill(kaiwaa, "");
-		  
+
 		  kaiwa[0]="私はアリス！蟹座の21歳☆";
 		  kaiwa[1]="今日は帽子屋さんから森のお茶会に誘われたけど";
 		 kaiwaa[1]="どうやら道に迷ってしまったみたい...。";
@@ -102,7 +102,7 @@ public class kaiwa implements State  {
 		  model.st.stop();
 			model.st.play();}
 	}
-	
+
 	public void processTimeElapsed(int msec){
 		if (model.i==9)
 			tekitime+=10;
@@ -117,7 +117,7 @@ public class kaiwa implements State  {
 	public State process(String event) {
 		if(event.charAt(0) == 'E'){
 			if(model.i==9&&tekitime<200 ){
-				
+
 			}
 			else {
 			sound=1;
@@ -126,8 +126,8 @@ public class kaiwa implements State  {
 			model.i++;
 
 			time=0;
-			
-			
+
+
 			}}
 		if(event.charAt(0) == ' '){
 			return new BossCome(this,model);
@@ -135,57 +135,58 @@ public class kaiwa implements State  {
 		if(event.charAt(0) == 's'){
 			if(model.i<=21){
 				aris.stop();
+				  setumei.stop();
 		return new PlayingState(model);}
 			else
 				return new RecordState(model);
-			
+
 		}
 		return this;
 		}
 
-	
-	
+
+
 	public void paint(Graphics g) {
-		
-		
+
+
 		g.setFont(new Font(Font.SERIF, Font.BOLD, 26));
        g.setColor(Color.PINK);
-     
-	
+
+
 		  g.drawImage(  haikei1, 0, 0,model.getView());
-		  
-		
-		  
+
+
+
 		  if(model.i<=1){
-			
+
 		  g.drawImage(  arisu, 0, 300,model.getView());
 		  nameflag=1;}
 		  if(model.i==2){
 		  g.drawImage(  arisukuro, 0, 300,model.getView());
 		  nameflag=3;}
-		  
+
 		  if(model.i==3){
 			  g.drawImage(  arisu, 0, 300,model.getView());
 		  nameflag=1;}
-		  
+
 		  if(model.i==4||model.i==6||model.i==8||model.i==11||model.i==12||model.i==14||model.i==15||model.i==16||model.i==17||model.i==19||model.i==23||model.i==25){
 			  g.drawImage(  bousi, 680, 240,model.getView());
 		  g.drawImage(  arisukuro, 0, 300,model.getView());
 		  nameflag=2;}
-		  
+
 		  if(model.i==5||model.i==7||model.i==10||model.i==13||model.i==18||model.i==20||model.i==22||model.i==24||model.i==26){
 		  g.drawImage(  bousikuro, 680, 240,model.getView());
 		  g.drawImage(  arisu, 0, 300,model.getView());
 		  nameflag=1;}
 		  if(model.i==27)
 			  nameflag=0;
-		  
+
 		  if(model.i==9||model.i==21){
 			  g.drawImage(  bousikuro, 680, 240,model.getView());
 		  g.drawImage(  arisukuro, 0, 300,model.getView());
 	nameflag=0;}
-		 
-		  if(model.i==9){ 
+
+		  if(model.i==9){
 			  g.drawImage( honey, tekitime, tekitime,model.getView());
 		  g.drawImage( honey, 500, tekitime,model.getView());
 		  g.drawImage( honey, 1000-tekitime, tekitime,model.getView());
@@ -204,30 +205,30 @@ public class kaiwa implements State  {
 		  g.setColor(Color.WHITE);
 			g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 45));
 		  if(model.i==15)  {
-			  
+
           g.drawString( "rifle", 480, 400);
           g.drawString( "hand", 480, 450);
           g.setColor(Color.RED);
          	 g.drawString( "▼", 420, 452);
           }
-		  
+
 		  if(model.i==16){
 		  g.drawImage( honey, 500, 350,model.getView());
 		  g.drawString( "honey",500, 480);
 			  }
 		  g.setFont(new Font(Font.SERIF, Font.PLAIN, 30));
 		  g.setColor(Color.PINK);
-		  
+
 		  g.drawImage(  waku, 80, 580,model.getView());
-		if (time>3){  
+		if (time>3){
 		  if(time%2==0){
 			  g.drawString("Enter", 950, 675);
 			  g.drawString("▼", 970, 700);}
 		  else {
-		 g.drawString("▼", 970, 710); 
+		 g.drawString("▼", 970, 710);
 		  g.drawString("Enter", 950, 685);}
 		}
-		  
+
 		if(nameflag==0)
 			  g.drawString("", 200, 620);
 		  if(nameflag==1)
@@ -236,35 +237,35 @@ public class kaiwa implements State  {
 		  g.drawString("帽子屋", 200, 620);
 		  else if(nameflag==3)
 		  g.drawString("？？？", 200, 620);
-		  
+
 		  if(model.i==19)
 			  g.drawImage( kaifuku, 500, 350,model.getView());
-			
+
 		  g.setColor(Color.WHITE);
-		  
+
 		  if(model.i==14) g.drawString(kaiwaa[model.i], 160, 703);
 		  else  g.drawString(kaiwaa[model.i], 170, 703);
-		
-	
-		  
-		
-		  
+
+
+
+
+
 		  g.drawString(  kaiwa[model.i], 170, 660);
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 25));
-		  g.drawString("'S'を押すとスキップするよ！", 680, 40); 
+		  g.drawString("'S'を押すとスキップするよ！", 680, 40);
 		}
-	
+
 	public State gamestate(){
 		if(model.i==21){
 			setumei.stop();
 			 return new PlayingState(model);
-		}	
-		
+		}
+
 		else	if(model.i==29){
 			aris.stop();
 			 return new RecordState(model);
-		}	
+		}
 		else	return this;}
-	
-	
+
+
 }
